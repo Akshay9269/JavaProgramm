61f1d7d8b050c5b71d32074d697550185708adf3
Akshay9269 <akshaybondarde9269@gmail.com>
2022-08-09 09:20:14 +0530

    First java program

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test {
+	
+	public static void main(String[] args) {
+	Scanner Scanner=new Scanner(System.in);
+		System.out.println("Hello World!");
+		int a;
+		a=5+10;
+		System.out.println(a);
+
+		System.out.println("Hello Akshata!");
+
+	}
+
+}
